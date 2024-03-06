df60f2bad3bed210999e4160aea28115547a0843
KhurramMalik15 <155771526+KhurramMalik15@users.noreply.github.com>
2024-03-06 19:41:13 +0500

    JavaScript Assignment-01

---
+++ JS_assignment_01/script.js
@@ -0,0 +1,11 @@
+// Prompt for first name
+let firstName = prompt("Enter your first name:");
+
+// Prompt for middle name
+let middleName = prompt("Enter your middle name:");
+
+// Prompt for last name
+let lastName = prompt("Enter your last name:");
+
+// Print the full name in the console
+console.log("Full Name:", firstName, middleName, lastName);
