2407a3950cff90d8013aa10d9d5ebb4c8a6f9f3b
KhurramMalik15 <155771526+KhurramMalik15@users.noreply.github.com>
2024-03-12 22:54:32 +0500

    JavaScript-Assignment-02

---
+++ JS_assignment_02/script.js
@@ -0,0 +1,33 @@
+/*
+let num1 = prompt("enter first number");
+let num2 = prompt("enter second number");
+let num3 = prompt("enter third number");
+
+console.log(num1 + num2 + num3);
+*/
+
+
+
+/*
+let num1 = prompt("enter first number");
+let num2 = prompt("enter second number");
+let num3 = prompt("enter third number");
+
+let convertNumber1 = parseInt(num1)
+let convertNumber2 = parseInt(num2)
+let convertNumber3 = parseInt(num3)
+
+console.log(convertNumber1 + convertNumber2 + convertNumber3);
+*/
+
+
+
+let num1 = parseInt(prompt("Enter the first number: "));
+let num2 = parseInt(prompt("Enter the second number: "));
+let num3 = parseInt(prompt("Enter the third number: "));
+
+let result = num1 + num2 + num3;
+
+
+console.log("The sum of the three numbers is:", result);
+
